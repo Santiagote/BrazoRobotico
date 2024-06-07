e484bcb639b2cea3a320f10c770ab7925f024046
Roberto Lliguin <166523151+RCL05@users.noreply.github.com>
2024-06-07 10:08:46 -0500

    Delete PaginaNexus/PaginaWebNexus directory

--- PaginaNexus/PaginaWebNexus/Controles/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-class Registro(models.Model):
-    usuario = models.CharField(max_length=100)
-    accion = models.CharField(max_length=255)
-
-    def __str__(self):
-        return f"{self.usuario} - {self.accion} "
-# Create your models here.
--- PaginaNexus/PaginaWebNexus/PaginaWebNexus/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def vista_grabar_objetos(request):
-    return None
-
-
-def vista_recoger_objetos(request):
-    return None
