2e2ca87d1c0ae527a78e24f3d8f27b296d212c78
RCL05 <roberto.lliguin@unl.edu.ec>
2024-06-07 09:03:47 -0500

    Archivos de la pagina web

---
+++ IngresoPaginaWeb/views.py
@@ -0,0 +1,36 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from Controles.camera import grabar_objetos
+from Controles.pinza import recoger_datos_objeto
+
+
+
+def home(request):
+    return render(request, 'index.html')
+
+
+def controles(request):
+    return render(request, 'controles.html')
+
+def vista_grabar_objetos(request):
+    grabar_objetos()
+    return HttpResponse('Grabacion de Objetos iniciada')
+
+def vista_recoger_objetos(request):
+    datos= recoger_datos_objeto()
+    return HttpResponse(f'Datos del objeto recogidos: {datos}')
+
+def descripcion(request):
+    return render(request, 'descripcion.html')
+
+
+def registro(request):
+   return render(request, 'registro.html')
+
+
+
+def Login(request):
+    return render(request, 'login.html')
+
+
---
+++ PaginaWebNexus/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+
+def vista_grabar_objetos(request):
+    return None
+
+
+def vista_recoger_objetos(request):
+    return None
